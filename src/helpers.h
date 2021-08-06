91762187a2f59558c590390ad1250098d5028fe8
barbarisch <bpeterson926@gmail.com>
2021-08-06 10:54:27 -0400

    bse64 obfuscation to output

--- src/helpers.h
+++ src/helpers.h
@@ -2,6 +2,8 @@
 #define SNAKE_HELPERS
 #include <time.h>
 
+#include "base64.h"
+
 #define debug(x...) fprintf(stderr,x)
 
 #define fatal(x...) { \
@@ -9,9 +11,27 @@ fprintf(stderr, "[-] ERROR: " x); \
 exit(1); \
 }\
 
-#define output(x...) { \
+#define output2(x...) { \
 fprintf(stderr, "[%s] %d %d %s\t", process_username, (int)time(0), process_pid, process_name);\
 fprintf(stderr, x);\
+}\
+
+#define output(x...) { \
+int i = 0;\
+char* tempbuf = calloc(1024, 1);\
+if(tempbuf != NULL) {\
+int ret = snprintf(tempbuf, 1022, "[%s] - %d - %d - %s\t", process_username, (int)time(0), process_pid, process_name);\
+ret = ret + snprintf(tempbuf+ret, 1022-ret, x);\
+for(i=0; i<ret; i++) {\
+tempbuf[i] = tempbuf[i] ^ 'a';\
+}\
+int len = 0;\
+char* anotherbuf = base64(tempbuf, ret, &len);\
+fprintf(stderr, "%s\n", anotherbuf);\
+free(tempbuf);\
+if(anotherbuf)\
+free(anotherbuf);\
+}\
 }\
 
 #endif
